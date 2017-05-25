43b1c23604a69637ccb2c6637f161971a1460cbc
Ajay Kumar <ajaykuma24@yahoo.com>
2017-05-24 21:42:12 -0500

    GPIO 'Done' Setup gpio using pigpio Should set pins 14, 15, 18, and 23 to appropriate pwm values from POSTed color values

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, send_from_directory, request
 from os import environ, path
 import json
+import pigpio
+
+gpio = pigpio.pi()
+
+red = 14
+green = 15
+blue = 18
+white = 23
+
+gpio.set_PWM_dutycycle(red, 0)
+gpio.set_PWM_dutycycle(green, 0) 
+gpio.set_PWM_dutycycle(blue, 0)
+gpio.set_PWM_dutycycle(white, 0) 
 
 here = path.abspath(path.dirname(__file__))
 
@@ -21,6 +34,9 @@ def setcolor():
 	R = d["R"]
 	G = d["G"]
 	B = d["B"]
+	gpio.set_PWM_dutycycle(red, R)
+	gpio.set_PWM_dutycycle(green, G) 
+	gpio.set_PWM_dutycycle(blue, B) 
 	print('R: ' + str(R) + ' G: ' + str(G) +' B: ' + str(B) )
 	return ('set!')
 
