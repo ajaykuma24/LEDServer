c7b3f3856eaf7fc4debada482176824dc54fd699
Ajay Kumar <ajaykuma24@yahoo.com>
2017-06-21 18:14:41 -0500

    Threading working with test values Need to implement frontend to create custom colors Implement infinite loops Determine how to process data to array of tuples format

--- app.py
+++ app.py
@@ -25,6 +25,9 @@ gpio.set_PWM_dutycycle(green, 0)
 gpio.set_PWM_dutycycle(blue, 0)
 gpio.set_PWM_dutycycle(white, 0) 
 
+runth = None
+stopt = threading.Event()
+
 testvals = [(150, 0  , 0  , 0, 500, 5000),
 			(150, 75 , 0  , 0, 500, 5000),
 			(75 , 75 , 0  , 0, 500, 5000),
@@ -37,34 +40,40 @@ testvals = [(150, 0  , 0  , 0, 500, 5000),
 			(75 , 0  , 150, 0, 500, 5000),
 			(75 , 0  , 75 , 0, 500, 5000),
 			(150, 0  , 75 , 0, 500, 5000),
-			(0  , 0  , 0 , 0, 500, 5000)]
+			(0  , 0  , 0  , 0, 500, 5000)]
 
 class FuncThread(threading.Thread):
-    def __init__(self, target, *args):
-        self._target = target
-        self._args = args
-        threading.Thread.__init__(self)
- 
-    def run(self):
-        self._target(*self._args)
+	def __init__(self, target, *args):
+		threading.Thread.__init__(self)
+		self._target = target
+		self._args = args
+
+	def stop(self):
+		stopt.set()
+
+	def run(self):
+		self._target(self._args)
 
 here = path.abspath(path.dirname(__file__))
 
 app = Flask(__name__, static_folder='templates')
 
 def clip(x, lo, hi):
-    return lo if x <= lo else hi if x >= hi else x
+	return lo if x <= lo else hi if x >= hi else x
 
 def setcols(R=0, G=0, B=0, W=0):
+	global currcolor
 	R = clip(R, 0.0, 255.0)
 	G = clip(G, 0.0, 255.0)
 	B = clip(B, 0.0, 255.0)
 	W = clip(W, 0.0, 255.0)
+	#print(str(R)+' '+str(G)+' '+str(B)+' '+str(W)+' ')
 
 	currcolor['r']=R
 	currcolor['g']=G
 	currcolor['b']=B
 	currcolor['w']=W
+	
 	R = round(R/radj)
 	G = round(G/gadj)
 	B = round(B/badj)
@@ -75,23 +84,24 @@ def setcols(R=0, G=0, B=0, W=0):
 	B = clip(B, 0.0, 255.0)
 	W = clip(W, 0.0, 255.0)
 
-	print(str(R)+' '+str(G)+' '+str(B)+' '+str(W)+' ')
-
 	gpio.set_PWM_dutycycle(red, R)
 	gpio.set_PWM_dutycycle(green, G) 
 	gpio.set_PWM_dutycycle(blue, B) 
 	gpio.set_PWM_dutycycle(white, W) 
 
 def change(R=0, G=0, B=0, W=0, wait=1000, changet = 2000):
+	global currcolor
 	diff = changet//delta
-	dr = (R-currcolor['r'])/diff
-	dg = (G-currcolor['g'])/diff
-	db = (B-currcolor['b'])/diff
-	dw = (W-currcolor['w'])/diff
-	for i in range(diff-1):
+	dr = (R-currcolor['r'])/float(diff)
+	dg = (G-currcolor['g'])/float(diff)
+	db = (B-currcolor['b'])/float(diff)
+	dw = (W-currcolor['w'])/float(diff)
+	for i in range(diff):
 		setcols(currcolor['r']+dr, currcolor['g']+dg, currcolor['b']+db, currcolor['w']+dw)
 		sleep(delta)
 	setcols(R, G, B, W)
+	if(stopt.is_set()):
+		return
 	sleep(wait)
 
 def sleep(s=1000):
@@ -99,6 +109,8 @@ def sleep(s=1000):
 
 def control(data):
 	for val in data:
+		if(stopt.is_set()):
+			return
 		change(*val)
 
 @app.route('/')
@@ -107,30 +119,49 @@ def hello_world():
 
 @app.route('/<path:filename>')
 def send_asset(filename):
-    print(path.join(here, '/templates/') + filename)
-    return send_from_directory(app.static_folder, filename)
+	print(path.join(here, '/templates/') + filename)
+	return send_from_directory(app.static_folder, filename)
 
 @app.route('/colors', methods=['POST'])
 def setcolor():
+	global runth
+	print('request')
 	d=request.get_json()
 	R = d["r"]
 	G = d["g"]
 	B = d["b"]
-	#change(R, G, B)
-	control(testvals)
+	change(R, G, B, 0, 500, 3000)
+	# if(not(runth is None)):	
+	# 	stopt.set()
+	# 	print('set')
+	# 	runth.join()
+	# 	print('joined')
+	# 	stopt.clear()
+	# print('first')
+
+	# print('test1')
+	# runth = FuncThread(control, *testvals)
+	# runth.start()
 	print('R: ' + str(R) + ' G: ' + str(G) +' B: ' + str(B) )
 	return ('set!')
 
 @app.route('/stop')
 def stopgpio():
+	global runth
 	gpio.set_PWM_dutycycle(red, 0)
 	gpio.set_PWM_dutycycle(green, 0) 
 	gpio.set_PWM_dutycycle(blue, 0)
 	gpio.set_PWM_dutycycle(white, 0) 
 	gpio.stop()
+	stopt.set()
+	runth.join()
 	return 'stopped'
 
+@app.route('/test')
+def testthread():
+	return 'test'
+
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True, threaded=True)
 
 
